b7780c6c90819266de72da46524a73ed9f4f2909
Juan Carlos Darias <106111059+nicproof@users.noreply.github.com>
2025-07-31 20:44:00 +0100

    Delete gestor-actividades-escolares_old directory

--- gestor-actividades-escolares_old/routes/api.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\Api\ActividadApiController;
-
-// Ruta pública: obtener todas las actividades
-Route::get('/actividades', [ActividadApiController::class, 'index']);
